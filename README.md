68a65b6ef2badb60d1c632ccab41727ee439083a
Malav Patel <mpatel636@login-ice-2.pace.gatech.edu>
2025-02-17 19:14:13 -0500

    removed auth tokens

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # modernpatentBERT
+
+## Environment Creation
+```bash
+conda env create -f environment.yaml # this should install flash attention by default
+
+conda activate mbertft
+```
+
+## Submitting A Test Job for Finetuning
+```bash
+# if necessary, export your huggingface authentication token
+export HF_TOKEN=<your token here>
+
+# optional, wandb login token
+export WANDB_TOKEN=<your token here>
+
+# ensure you are in the directory where the .sbatch file is located
+srun sbatch test.sbatch
+```
---
+++ finetune.py
@@ -0,0 +1,146 @@
+# to start jupyter server
+# salloc -G <GPU name here>:1
+
+# conda activate mberft
+# jupyter notebook --ip=0.0.0.0 --port=8888
+
+import os
+import torch
+import numpy as np
+import torch
+
+from sklearn.preprocessing import LabelEncoder
+
+from huggingface_hub import login
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    DataCollatorWithPadding,
+    TrainingArguments,
+    Trainer
+)
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+hf_token = os.getenv("HF_TOKEN")
+patents = load_dataset("MalavP/USPTO-3M", split="train", use_auth_token = hf_token)
+# Split the dataset: 90% for "dummy" (discarded), 10% for the subset
+mini_patents = patents.train_test_split(
+    test_size=0.01,  # 10% of the original data
+    shuffle=True,   # Randomize selection
+    seed=42         # For reproducibility
+)["test"]           # Keep the 10% test split
+
+mini_patents = mini_patents.rename_column("cpc_ids", "labels")
+mini_patents = mini_patents.map(lambda x: {'labels': x['labels'].split(',')})
+unique_labels = set(item for sublist in mini_patents["labels"] for item in sublist)
+
+
+# Initialize and fit the LabelEncoder
+label_encoder = LabelEncoder()
+label_encoder.fit(list(unique_labels))
+
+def convert_labels(example):
+    indices =  [label_encoder.transform([label])[0] for label in example['labels']]
+    example["labels"] = [float(i in indices) for i in range(len(unique_labels))] # BCELossWithLogits requires target to contain floating point labels
+    return example
+
+# Apply the transformation to the dataset
+mini_patents = mini_patents.map(convert_labels)
+split_datasets = mini_patents.train_test_split(test_size=0.05, seed=42)
+train_dataset = split_datasets["train"]
+eval_dataset = split_datasets["test"]
+
+# Verify the size
+# print(f"Original size: {len(patents)}, Subset size: {len(mini_patents)}")
+
+# Model id to load the tokenizer
+model_id = "answerdotai/ModernBERT-base"
+# Load Tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+ 
+# Tokenize helper function
+def tokenize(batch):
+    # return tokenizer(batch['text'], padding='max_length', truncation=True, return_tensors="pt")
+    return tokenizer(batch['text'], truncation=True, padding=True, max_length=1024, return_tensors="pt")
+
+# Tokenize dataset
+train_dataset = train_dataset.map(tokenize, batched=True)
+eval_dataset = eval_dataset.map(tokenize, batched=True)
+
+hf_data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+# 6. Load the model for sequence classification (adjust num_labels as needed).
+model = AutoModelForSequenceClassification.from_pretrained(model_id, num_labels=len(unique_labels)).to(device)
+model.config.problem_type= "multi_label_classification"
+
+
+# 7. Define a compute_metrics function to compute accuracy.
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred # shape (N, num classes) and shape (N, num classes)
+    pred_labels = np.argmax(predictions, axis=-1)  # shape (N,)
+
+    # Assuming labels are lists of labels, check if pred_labels are in any of the true labels
+    correct = 0
+    total = len(labels)
+
+    for pred, true_labels in zip(pred_labels, labels):
+        correct += true_labels[pred]
+
+    # Accuracy calculation
+    accuracy = correct / total
+    return {"accuracy": accuracy}
+
+
+# Set your hyperparameters and task identifier
+train_bsz, val_bsz = 32, 32
+lr = 8e-5 # the authors use 2e-5
+betas = (0.9, 0.98)
+n_epochs = 1
+eps = 1e-6
+wd = 8e-6
+task = "your_task_name"  # Set this to an appropriate identifier for your task
+# 8. Define training arguments using your specified hyperparameters.
+training_args = TrainingArguments(
+    output_dir=f"aai_ModernBERT_{task}_ft",
+    learning_rate=lr,
+    per_device_train_batch_size=train_bsz,
+    per_device_eval_batch_size=val_bsz,
+    num_train_epochs=n_epochs,
+    lr_scheduler_type="linear",
+    optim="adamw_torch",
+    adam_beta1=betas[0],
+    adam_beta2=betas[1],
+    adam_epsilon=eps,
+    weight_decay=wd,
+    logging_strategy="epoch",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    bf16=True,
+    bf16_full_eval=True,
+    push_to_hub=False,
+    disable_tqdm=True,
+)
+
+
+
+# 9. Create the Trainer with the compute_metrics function.
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    data_collator=hf_data_collator,
+    compute_metrics=compute_metrics
+)
+
+wandb_token = os.getenv("WANDB_TOKEN")
+if wandb_token is not None:
+    import wandb
+    wandb.login(key=wandb_token)
+
+trainer.train()
